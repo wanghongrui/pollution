1e82729775e1e7b6747ab35809ba84e6b254eb63
wanghongrui <wanghongruigood@qq.com>
2021-01-01 20:25:32 +0800

    :tada: init commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Head msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Head from './components/Head.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Head
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+<style>
+:root {
+  background: #0f2a42;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 16px;
 }
 </style>
---
+++ src/components/Head.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="head">
+    <div class="head-logo"></div>
+    <div class="head-text">污染源大屏展示</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.head {
+  display: flex;
+  align-items: center;
+
+  &-logo {
+    width: 42px;
+    height: 42px;
+    background: url("../assets/logo.png");
+    background-size: contain;
+  }
+
+  &-text {
+    font-size: 2rem;
+  }
+}
+</style>
