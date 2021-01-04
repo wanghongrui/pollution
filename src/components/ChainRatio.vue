0c875d27571355f78553dbfe0c3890cf6e583a32
bibichuan <741841403@qq.com>
2021-01-04 23:17:46 +0800

    增加grid

--- src/components/ChainRatio.vue
+++ src/components/ChainRatio.vue
@@ -39,12 +39,12 @@ const chart_option = {
 		}
 	},
 
- 	grid:{
-		x: 10, //默认是80px
-		y: 10, //默认是60px
-		x2: 10, //默认80px
-		y2: 10 //默认60px
-	},
+ 	// grid:{
+	// 	x: 10, //默认是80px
+	// 	y: 10, //默认是60px
+	// 	x2: 10, //默认80px
+	// 	y2: 10 //默认60px
+	// },
 
 	series: [{
 		name: '访问来源',
@@ -191,8 +191,8 @@ export default {
 	top: 50px;
 
 	.ration_chart{
-		width:200px;
-		height: 100px;
+		min-width:200px;
+		min-height: 200px;
 	}
 	.number-tip-wrap{
 		display: flex;
