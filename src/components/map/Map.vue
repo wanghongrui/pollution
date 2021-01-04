a3630fbd80d9a5629e634d073201fc998d1c69b3
wanghongrui <wanghongruigood@qq.com>
2021-01-04 20:22:26 +0800

    :art: 数据图层

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -13,12 +13,80 @@
 <script>
 import Mapbox from "./Mapbox";
 import Styles from "./Styles";
+import loader from "load-script-promise";
+
+/**
+ * 1.土壤详查重点行业企业
+ * 2.场地调查及修复地块
+ * 3.重点监管排污企业
+ * 4.危废经营和产生企业
+ */
 
 export default {
   components: {
     Mapbox,
     Styles,
-  }
+  },
+  computed: {
+    data2() {
+      return this.$store.state.data2;
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.init();
+    }, 1000);
+  },
+  watch: {
+    data2() {
+      this.setData2();
+    },
+  },
+  methods: {
+    init() {
+      loader.loadScript("./data/data2.js").then(() => {
+        this.$store.commit("data2_changed", window.data2);
+      });
+    },
+    setData2() {
+      if (!this.data2) {
+        return;
+      }
+
+      this.addLayer(this.data2);
+    },
+    addLayer(data) {
+      const dataSource = map.getSource("data");
+
+      if (dataSource) {
+        dataSource.setData(data);
+      } else {
+        map.addSource("data", {
+          type: "geojson",
+          data,
+        });
+        map.addLayer({
+          id: "data",
+          type: "fill",
+          source: "data",
+          layout: {},
+          paint: {
+            "fill-color": "#088",
+            "fill-opacity": 0.8,
+          },
+        });
+
+        map.on("click", "data", (e) => {
+          const id = e.features[0].properties.OBJECTID;
+          data.features.forEach((f) => {
+            if (f.properties.OBJECTID === id) {
+              this.$store.commit("item_changed", f);
+            }
+          });
+        });
+      }
+    },
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    styledata: null
+    styledata: null,
+    data2: null,
+    item: null
   },
   mutations: {
-    styledata_changed (state, styledata) {
+    styledata_changed(state, styledata) {
       state.styledata = styledata
+    },
+    data2_changed(state, data2) {
+      state.data2 = data2
+    },
+    item_changed(state, item) {
+      state.item = item
     }
   },
   actions: {
